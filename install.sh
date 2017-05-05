0478a68fe2e7059800611c75a989fbe449971f2c
kady <elkadyabdelrahman@gmail.com>
2017-05-05 22:37:03 +0200

    Fix extraction dir

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo 'Downloading Fuseki, that could take some time ~40+Mb'
 wget http://www-eu.apache.org/dist/jena/binaries/apache-jena-fuseki-2.5.0.tar.gz
 
 cp apache-jena-fuseki-2.5.0.tar.gz server/lib
-tar -xzvf server/lib/apache-jena-fuseki-2.5.0.tar.gz
+tar -xzvf server/lib/apache-jena-fuseki-2.5.0.tar.gz -C server/lib/
 
 chmod +x server/lib/apache-jena-fuseki-2.5.0/fuseki-server
 chmod +x server/lib/apache-jena-fuseki-2.5.0/bin/s-*
